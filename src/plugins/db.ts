f97ad2649fcc3e095d78aea53cef4cb5dd37baf4
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2024-09-15 16:20:05 -0600

    feat(auth): add user table schema definition

--- src/plugins/db.ts
+++ src/plugins/db.ts
@@ -4,16 +4,24 @@ import { fastifyPlugin } from 'fastify-plugin';
 import postgres from 'postgres';
 
 import type { Config } from '../../config/config.d.ts';
+import { users } from '../schema/user.js';
 
 declare module 'fastify' {
   interface FastifyInstance {
-    db: ReturnType<typeof drizzle>;
+    db: ReturnType<typeof drizzle<{ users: typeof users }>>;
   }
 }
 
 const db: FastifyPluginCallback<Config> = (fastify, options, done) => {
   const client = postgres(options.db);
-  const db = drizzle(client);
+  const db = drizzle(client, {
+    schema: { users },
+    logger: {
+      logQuery(query, params) {
+        fastify.log.debug({ query, params });
+      },
+    },
+  });
 
   fastify.decorate('db', db);
 
