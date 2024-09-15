604b7bde91be9d372e68928169348022ec8794e3
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2024-09-14 19:51:01 -0600

    feat: setup OpenAPI documentation
    Install @fastify/swagger and @fastify/swagger-ui

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import { fastifyAutoload } from '@fastify/autoload';
 import { fastifySensible } from '@fastify/sensible';
+import { fastifySwagger } from '@fastify/swagger';
+import { fastifySwaggerUi } from '@fastify/swagger-ui';
 import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
 import fastify from 'fastify';
 
@@ -13,7 +15,38 @@ export const app = fastify({
     : !process.argv.some((arg) => arg.includes('test')),
 }).withTypeProvider<TypeBoxTypeProvider>();
 
-app.register(fastifySensible);
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'RealWorld Conduit API',
+      description: 'Conduit API documentation',
+      contact: {
+        name: 'RealWorld',
+        url: 'https://www.realworld.how',
+      },
+      license: {
+        name: 'MIT License',
+        url: 'https://opensource.org/licenses/MIT',
+      },
+      version: '2.0.0',
+    },
+    servers: [{ url: 'http://localhost:1337' }],
+    tags: [
+      { name: 'Articles' },
+      { name: 'Comments' },
+      { name: 'Favorites' },
+      { name: 'Profile' },
+      { name: 'Tags' },
+      { name: 'User and Authentication' },
+    ],
+  },
+});
+
+app.register(fastifySwaggerUi);
+
+app.register(fastifySensible, {
+  sharedSchemaId: 'HttpError',
+});
 
 app.register(fastifyAutoload, {
   dir: `${import.meta.dirname}/routes`,
