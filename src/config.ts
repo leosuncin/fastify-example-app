8ce4e10e7b32bf526b69147bc30473e3ffb89b6e
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2024-09-17 21:35:52 -0600

    refactor(auth): create a function to setup the app
    Pass configuration to the build function

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,11 @@ import StrongConfig from '@strong-config/node';
 
 import type { Config } from '../config/config.d.ts';
 
-process.env.NODE_ENV ??= 'development';
+if (process.argv.some((arg) => arg.includes('test'))) {
+  process.env.NODE_ENV = 'test';
+} else {
+  process.env.NODE_ENV ??= 'development';
+}
 
 const strongConfig = new StrongConfig();
 
